0e511a1fcea7cd61e08e3edb423e05e622cb4d53
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-02-22 20:27:58 +0530

    gsap added

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function Header() {
+  useGSAP(() => {
+    // gsap code here...
+    gsap.from(".left", { x: -440, rotate: 360 });
+  });
   return (
     <div className="w-full h-[60px] bg-gray-900 text-white p-3 flex justify-between items-center">
       <Link to="/">
-        <h2 className="font-bold text-amber-400 ">Web Compiler</h2>
+        <h2 className="font-bold left text-amber-400 ">Web Compiler</h2>
       </Link>
       <ul className="flex gap-2">
         <li>
           <Link to="/compiler">
             <Button
-              className=" animate-ping text-amber-400 animate-bounce font-semibold"
+              className="font-semibold animate-ping text-amber-400 animate-bounce"
               variant="outline"
             >
               Compiler
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,16 +2,29 @@ import { Canvas } from "@react-three/fiber";
 import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 import Vishwajit from "@/models/Vishwajit.jsx";
 import { Suspense } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function Home() {
+  useGSAP(() => {
+    // gsap code here...
+    gsap.from(".title", { y: -10000, stagger: 0.3, ease: "power1.in" });
+    gsap.from(".update", { y: 10000, duration: 1 });
+    gsap.from(".updatext", { y: 10000, duration: 3, stagger: 0.5 });
+  });
   return (
     <div>
-      <div className="title z-10 animate-pulse  flex justify-center text-5xl pt-3 mt-6 font-bold animate-slideIn">
-        Welcome to My Web Compiler
+      <div className="z-10 flex justify-center gap-2 pt-3 mt-6 text-5xl font-bold animate-pulse animate-slideIn">
+        <div className="title">Welcome</div>
+        <div className="title">to</div>
+        <div className="title">My</div>
+        <div className="title">Web</div>
+        <div className="title">Compiler</div>
       </div>
-      <h2 className=" text-emerald-300  text-xl text-center font-serif mt-3">
+      <h2 className="mt-3 font-serif text-xl text-center update text-emerald-300">
         Upcoming Update
-        <div className="text-sm mt-1">Threejs Animation</div>
+        <div className="mt-1 text-sm updatext">Threejs Animation</div>
+        <div className="mt-1 text-sm updatext">More Animations</div>
       </h2>
 
       <div className="mt-[6vh] z-0 bg-white h-[90vh] flex justify-center">
